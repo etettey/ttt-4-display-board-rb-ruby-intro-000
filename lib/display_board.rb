22d2459f948604d9015da677f3adb9fcb74fc746
resourceful-role-9993 <unicetee@gmail.com>
2020-03-06 13:11:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,8 @@
 def display_board (board)
   puts "   |   |   "
   puts "-----------"
-  puts "   | X  |   "
+  puts "   |   |   "
   puts "-----------"
-puts "   |   |   "
+  puts "   |   |   "
 end
 display_board board=[" "," "," "," X"," "," "," "," "," "]
